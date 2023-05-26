e7f9b3fc8e160b0a0ee6100011c02e17ec5b44fa
chapetta <cha.petta@hotmail.com>
2023-05-26 20:03:19 -0300

    Done: requisito 3 - Desenvolva testes de no mínimo 50% de cobertura total e 100% da função fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,23 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('teste se a funçao fetchItem é uma funçao', () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+  it('Teste se a funçao fetchItem com o argumento "MLB1615760527" é chamada', () => {
+    fetchItem('MLB1615760527')
+
+    expect(fetch).toBeCalled();
+  });
+  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint https://api.mercadolibre.com/items/MLB1615760527', () => {
+    const url =  'https://api.mercadolibre.com/items/MLB1615760527'
+    fetchItem('MLB1615760527')
+
+    expect(fetch).toBeCalledWith(url)
+  });
+  it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
+    
+    const expected = await fetchItem('MLB1615760527')
+    expect(expected).toEqual(item)
+  })
 });
