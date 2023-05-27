464c05b1ff2d765964a29086b458a56fb54e5d9b
chapetta <cha.petta@hotmail.com>
2023-05-27 16:45:47 -0300

    Done: requisito 6 - Desenvolva testes de no mínimo 75% de cobertura total e 100% da função saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,9 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    const cartItem = '<ol><li>Item</li></ol>'
+    saveCartItems(cartItem);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  })
 });
