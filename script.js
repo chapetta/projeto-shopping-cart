47004b19c4ee976e30a52a11d053793e3e138d51
chapetta <cha.petta@hotmail.com>
2023-05-30 18:28:19 -0300

    Done: requisito 9 -  Calcule o valor total dos itens do carrinho de compras

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const ol = document.querySelector('.cart__items');
+const totalValue = document.getElementsByClassName('total-price')[0];
+
+const getTotalPrice = () => {
+  const savedTotalPrice = localStorage.getItem('totalPrice');
+  return savedTotalPrice ? parseFloat(savedTotalPrice) : 0;
+};
+
+let totalPrice = getTotalPrice();
+
+const saveTotalPrice = (price) => {
+  localStorage.setItem('totalPrice', price);
+};
 
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
@@ -31,8 +43,15 @@ const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').inn
 
 const cartItemClickListener = ({ target }) => {
   const item = target;
+  const priceElement = item.innerText;
+  const priceRegex = /PRICE: \$(\d+(\.\d{1,2})?)/;
+  const matches = priceElement.match(priceRegex);
+  const price = parseFloat(matches[1]);
+  totalPrice -= price; // Subtract the price of the removed item
   item.remove();
   saveCartItems(ol.innerHTML);
+  totalValue.innerText = `Total: $${totalPrice.toFixed(2)}`;
+  saveTotalPrice(totalPrice.toFixed(2));
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
@@ -59,10 +78,13 @@ const insertItemCart = async () => {
     element.addEventListener('click', async () => {
       const itemId = getSkuFromProductItem(element);
       const { id, title, price } = await fetchItem(itemId);
-      console.log(id, title, price);
       const itemCart = createCartItemElement({ sku: id, name: title, salePrice: price });
+      totalPrice += price;
       ol.appendChild(itemCart);
       saveCartItems(ol.innerHTML);
+
+      totalValue.innerText = `Total: $${totalPrice.toFixed(2)}`;
+      saveTotalPrice(totalPrice.toFixed(2));
     });
   });
 };
@@ -71,6 +93,19 @@ const getLocalStorage = () => {
   const reload = getSavedCartItems();
   ol.innerHTML = reload;
   ol.childNodes.forEach((child) => child.addEventListener('click', cartItemClickListener));
+
+  totalPrice = 0; // Reset the total price
+
+  // Iterate over each cart item and calculate the total price
+  ol.childNodes.forEach((child) => {
+    const priceElement = child.innerText;
+    const priceRegex = /PRICE: \$(\d+(\.\d{1,2})?)/;
+    const matches = priceElement.match(priceRegex);
+    const price = parseFloat(matches[1]);
+    totalPrice += price;
+  });
+  totalValue.innerText = `Total: $${totalPrice.toFixed(2)}`;
+  saveTotalPrice(totalPrice.toFixed(2));
 };
 
 window.onload = async () => {
@@ -78,4 +113,6 @@ window.onload = async () => {
   await insertItemCart();
   getLocalStorage();
   cartItemClickListener();
+  totalPrice = getTotalPrice();
+  totalValue.innerText = `Total: $${totalPrice.toFixed(2)}`;
 };
